173215d688af5b41f10fd396c2447cced297b8f0
Shaun Williams <shaun.e.williams@gmail.com>
2018-09-30 20:25:37 -0500

    create initial cells

---
+++ tetris-redux/index.js
@@ -0,0 +1,70 @@
+const UP = 0;
+const RIGHT = 1;
+const DOWN = 2;
+const LEFT = 3;
+
+const numRows = 9;
+const numCols = 5;
+
+const ghostRow = 3;
+
+function makeCell({ x, y }) {
+  return {
+    x,
+    y,
+    filled: false,
+    connect: [false, false, false, false],
+    next: {},
+    no: undefined,
+    group: undefined
+  };
+}
+
+function setGhostHomeCells(table) {
+  let c;
+  c = table[ghostRow][0];
+  c.filled = true;
+  c.connect[LEFT] = c.connect[RIGHT] = c.connect[DOWN] = true;
+
+  c = table[ghostRow][1];
+  c.filled = true;
+  c.connect[LEFT] = c.connect[DOWN] = true;
+
+  c = table[ghostRow + 1][0];
+  c.filled = true;
+  c.connect[LEFT] = c.connect[UP] = c.connect[RIGHT] = true;
+
+  c = table[ghostRow + 1][1];
+  c.filled = true;
+  c.connect[UP] = c.connect[LEFT] = true;
+}
+
+function makeCells() {
+  // initialize cells
+  const table = [];
+  const cells = [];
+  for (let y = 0; y < numRows; y++) {
+    const row = [];
+    for (let x = 0; x < numCols; x++) {
+      const cell = makeCell({ x, y });
+      cells.push(cell);
+      row.push(cell);
+    }
+    table.push(row);
+  }
+
+  // allow each cell to refer to surround cells by direction
+  for (const cell of cells) {
+    const { x, y } = cell;
+    if (x > 0) cell.next[LEFT] = table[y][x - 1];
+    if (x < numCols - 1) cell.next[RIGHT] = table[y][x + 1];
+    if (y > 0) cell.next[UP] = table[y - 1][x];
+    if (y < numRows - 1) cell.next[DOWN] = table[y + 1][x];
+  }
+
+  setGhostHomeCells(table);
+
+  return { table, cells };
+}
+
+console.log(makeCells());
